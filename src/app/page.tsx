496968ac4ef2ed428bb314d0ec824b1a9376b062
010binary <augustine32fg@gmail.com>
2024-05-13 03:15:36 +0100

    fixed the issue with tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,21 @@ import {
   Navbar,
   ProjectsSection,
   SkillsSection,
-  ContactSection
+  ContactSection,
 } from "@/component";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#252525] container mx-auto">
+    <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
-      <HeroSection />
-      <AboutSection />
-      <MissionSection />
-      <ProjectsSection />
-      <SkillsSection />
-      <ContactSection />
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <HeroSection />
+        <AboutSection />
+        <MissionSection />
+        <ProjectsSection />
+        <SkillsSection />
+        <ContactSection />
+      </div>
       <FooterSection />
     </main>
   );
